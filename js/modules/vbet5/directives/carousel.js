60dd87b4ed701afe2a145ad6a67893c729fccaeb
Arik <tonoyandeveloper@gmail.com>
2017-05-03 03:52:01 +0300

    Fix issue with carousel , reInit on resize

--- js/modules/vbet5/directives/carousel.js
+++ js/modules/vbet5/directives/carousel.js
@@ -19,16 +19,18 @@ var defaultOptions = {
 	backward: 1,
 };
 
-var sly = new Sly(document.getElementsByClassName('frame')); 
+var sly = new Sly(document.getElementById('frame')); 
 
 VBET5.directive('slyHorizontalRepeat', [
   '$timeout',
   function ($timeout) {
     return {
       restrict: 'A',
-      link: function (scope, el, attrs) {      
+      link: function (scope, el, attrs) {
+
         if (scope.$last === true) {
           $timeout(function () {
+            var cont = $('#frame')
             var frame = $(el[0]).parent().parent();
             var wrap = $(el[0]).parent().parent().parent();
             defaultOptions.horizontal = 1;
@@ -45,32 +47,65 @@ VBET5.directive('slyHorizontalRepeat', [
             // Merge parts into options object for sly argument
             var options = $.extend({}, defaultOptions, defaultControls, scope.$eval(attrs.slyOptions));
             var callback = scope.$eval(attrs.slyCallback) || function (cb) {
-              };
+            };
+            $(window).on('resize', function () {
+              frame.sly('reload');
+              console.log('resize');
+            });
             // Call Sly on frame
             frame.sly(options, callback());  //         scope.$emit('ngRepeatFinished');
           });
         }
-
       }
     };
   }
 ]);
 
+//METHODS
 //Positioning
-VBET5.directive('slyToBegin', function(){ //slyToStart doesnt seem to work :( 
-	return {
-		restrict: 'A',
-		link: function (scope, el, attrs){
-			el.on('click', function () {
-				// Need to pass the sly frame element Id
-				var frame = $('#'+attrs.slyFrame);
-				var item = attrs.slyDataItem || undefined;
-				
-				// Animate a particular item to the start of the frame.
-				// If no item is provided, the whole content will be animated.
-				frame.sly('toStart', item);
-			});
-
-		}
-	}
+VBET5.directive('slyToBegin', function () {
+  //slyToStart doesnt seem to work :( 
+  return {
+    restrict: 'A',
+    link: function (scope, el, attrs) {
+      el.on('click', function () {
+        // Need to pass the sly frame element Id
+        var frame = $('#' + attrs.slyFrame);
+        var item = attrs.slyDataItem || undefined;
+        // Animate a particular item to the start of the frame.
+        // If no item is provided, the whole content will be animated.
+        frame.sly('toStart', item);
+      });
+    }
+  };
+});
+VBET5.directive('slyToCenter', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, el, attrs) {
+      el.on('click', function () {
+        // Need to pass the sly frame element Id
+        var frame = $('#' + attrs.slyFrame);
+        var item = attrs.slyDataItem || undefined;
+        // Animate a particular item to the center of the frame.
+        // If no item is provided, the whole content will be animated.
+        frame.sly('toCenter', item);
+      });
+    }
+  };
+});
+VBET5.directive('slyToEnd', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, el, attrs) {
+      el.on('click', function () {
+        // Need to pass the sly frame element Id
+        var frame = $('#' + attrs.slyFrame);
+        var item = attrs.slyDataItem || undefined;
+        // Animate a particular item to the center of the frame.
+        // If no item is provided, the whole content will be animated.
+        frame.sly('toEnd', item);
+      });
+    }
+  };
 });

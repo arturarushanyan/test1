6e89a6026aad92840c547b308e3ce39f04e58b40
Tigran Sargsyan <tigran.sargsyan@betconstruct.com>
2017-06-06 21:32:16 +0400

    delete project

--- js/modules/vbet5/controllers/sportsbookswitchers.js
+++
@@ -1,4 +0,0 @@
-VBET5.controller('sportsbookSwitchersCtrl', ['$scope', '$rootScope', 'Utils', 'Config', function ($scope, $rootScope, Utils, Config) {
-    'use strict';
-    $scope.sportsbookAvailableViews = Utils.checkForAvailableSportsbookViews(Config);
-}]);
